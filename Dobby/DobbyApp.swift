baa2c2a93dad8b5751bcfc6ed1e6ef985eaaa6a1
Pielie <gilles.1998.moenaert@gmail.com>
2026-01-18 20:17:49 +0100

    Initial Commit

---
+++ Dobby/DobbyApp.swift
@@ -0,0 +1,17 @@
+//
+//  DobbyApp.swift
+//  Dobby
+//
+//  Created by Gilles Moenaert on 18/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct DobbyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
